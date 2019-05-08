d21ac2dbaf5aeaf48fce2a367cedd3f809d19e76
tlqiao <qiaotl@163.com>
2019-05-08 14:27:58 +0800

    add mock file

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,7 +5,7 @@ import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
 public class Application {
-    private static final int PORT = 8080;
+    private static final int PORT = 9090;
     private static final  String LOCALHOST= "localhost";
     private static final String MOCK_DIR= "mock";
 
